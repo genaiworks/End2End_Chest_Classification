208683a95dd30bdf0a61470ae2e90d43c0c2f2c1
genaiworks <genaiworks@gmail.com>
2024-04-23 02:20:44 -0700

    prepare base model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME="Data Ingestion Stage"
 
@@ -11,4 +12,12 @@ try:
 except Exception as e:
     raise e
 
+STAGE_NAME="Prepare Base Model Stage"
 
+try:
+    logger.info(f"******* stage {STAGE_NAME} started")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f"******** stage {STAGE_NAME} completed")
+except Exception as e:
+        raise e
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -3,7 +3,7 @@ from cnnClassifier.utils.common import read_yaml,create_directories
 from cnnClassifier import logger
 import gdown
 import zipfile
-from cnnClassifier.entity.config_entity import DataIngestionConfig
+from cnnClassifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig)
 
 class ConfigurationManager:
     def __init__(self, config_file_path=CONFIG_FILE_PATH, params_filepath=PARAMS_FILE_PATH):
@@ -21,3 +21,18 @@ class ConfigurationManager:
             unzip_dir=config.unzip_dir
         )
         return data_ingestion_config
+    
+    def get_prepare_base_model_config(self)-> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        create_directories([config.root_dir])
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weight=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES,
+        )
+        return prepare_base_model_config
